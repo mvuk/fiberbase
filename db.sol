b19cff8dfae68789483ad9a13272ba0e602c0ea5
matt <matt@eco>
2017-12-29 15:49:53 -0500

    added comments

--- db.sol
+++ db.sol
@@ -3,14 +3,17 @@ pragma solidity ^0.4.15;
 //describes the whole program
 contract FiberContract {
 
+  // set the variables
   string ipfsHash;
   string title;
 
+  // inputs, one single contract asks for IPFS hash and title
   function set(string _ipfsHash, string _title) {
     ipfsHash = _ipfsHash;
     title = _title;
   }
 
+  // display the result as a pair
   function get() constant returns (string _ipfsHash, string _title) {
     return (ipfsHash, title);
   }
