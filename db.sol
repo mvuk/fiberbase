ebd3a65303afb7736b972759bedf8f06ce320353
matt <matt@eco>
2017-12-29 15:09:41 -0500

    structs, init function and write record

--- db.sol
+++ db.sol
@@ -1,11 +1,36 @@
 paragma solidity ^0.4.16
 
+//describes the whole program
 contract FiberContract {
 
+  //what is being written to db
   struct Record {
     address user;
     string ipfsHash;
+    string title;
   }
+
+  //feedback to user ...?
+  struct OutputDB {
+    uint numberOfEntries;
+  }
+
+  //for creation of db
   address initiatior;
   mapping (address => Record) records;
+  OutputDB[] outputDBs;
+
+  //initiate contract
+  function Create() public {
+    address = msg.sender;
+  }
+
+  //create a new record
+  function WriteRecord(address user, string _ipfsHash, string _title) public {
+    user = msg.sender;
+    ipfsHash = _ipfsHash;
+    title = _title
+
+  }
+
 }
