9a35514fdeb90e6e6b221bc0f7a043a88a45e30b
matt <matt@eco>
2017-12-29 15:37:43 -0500

    ipfs hash successful contract to write to database; contract ID: QmaKQcsu9QUyXGnmUgbxCLy6XBvXM6ub1DCq7xALanGwzm

--- db.sol
+++ db.sol
@@ -3,34 +3,17 @@ paragma solidity ^0.4.16
 //describes the whole program
 contract FiberContract {
 
-  //what is being written to db
-  struct Record {
-    address user;
-    string ipfsHash;
-    string title;
+  string ipfsHash;
+  string title;
+
+  function set(string x, string y) {
+    ipfsHash = x;
+    title = y;
   }
 
-  //feedback to user ...?
-  struct OutputDB {
-    uint numberOfEntries;
-  }
-
-  //for creation of db
-  address initiatior;
-  mapping (address => Record) records;
-  OutputDB[] outputDBs;
-
-  //initiate contract
-  function Create() public {
-    address = msg.sender;
-  }
-
-  //create a new record
-  function WriteRecord(address user, string _ipfsHash, string _title) public {
-    user = msg.sender;
-    ipfsHash = _ipfsHash;
-    title = _title
-
+  function get() constant returns (string x, string y) {
+    return ipfsHash;
+    return title;
   }
 
 }
