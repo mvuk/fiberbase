47c937ac9bb96f725fe0491c2b3d6fba98c23e48
Jesse Abramowitz <jesseabramowitz@hotmail.com>
2017-12-30 11:27:22 -0500

    created owner for contract

--- db.sol
+++ db.sol
@@ -1,11 +1,22 @@
 pragma solidity ^0.4.15;
 
+//set the owner of the contract
+contract owned {
+  address public owner;
+
+  function owned () public {
+    owner = msg.sender;
+  }
+}
+
+
 //describes the whole program
 contract FiberContract {
 
   // set the variables
   string ipfsHash;
   string title;
+
 
   // inputs, one single contract asks for IPFS hash and title
   function set(string _ipfsHash, string _title) {
@@ -14,7 +25,8 @@ contract FiberContract {
   }
 
   // display the result as a pair
-  function get() constant returns (string _ipfsHash, string _title) {
+  function get() returns (string _ipfsHash, string _title) {
+
     return (ipfsHash, title);
   }
 
